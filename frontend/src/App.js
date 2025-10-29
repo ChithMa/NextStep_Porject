7160697601087089cfc6c286f3f26037df200c98
Sukitha Chathuranga Hettimudalige <sukithachathuranga@gmail.com>
2025-10-30 00:56:54 +0530

    Add Nodemon to the Project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './components/login';
+import Login from './components/Login';
 import Step1 from './components/Register/Step1';
 import Step2 from './components/Register/Step2';
 import Step3 from './components/Register/Step3';
